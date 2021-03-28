a07eef582851bfb10065fc9b26e9857c3d16312c
Kevin Xu <58110712+kevinxuht@users.noreply.github.com>
2021-03-28 05:05:10 +0100

    Update rank_order.py

--- rank_order.py
+++ rank_order.py
@@ -13,10 +13,10 @@ def rank_order():
 
     for challenge in PUBLIC_CHALLENGES:
         if challenge == "ESG" or challenge == "Data Cleaning":
-            df[f"{challenge} - public"] = df[f"{challenge} - public"].apply(lambda x: np.round(x, 3))
+            df[f"{challenge} - public"] = df[f"{challenge} - public"].apply(lambda x: np.round(x, 7))
             df[f"{challenge} - rank"] = df[f"{challenge} - public"].rank(ascending=True).apply(lambda x: int(x) if not np.isnan(x) else 50)
         else:
-            df[f"{challenge} - public"] = df[f"{challenge} - public"].apply(lambda x: np.round(x, 4))
+            df[f"{challenge} - public"] = df[f"{challenge} - public"].apply(lambda x: np.round(x, 7))
             df[f"{challenge} - rank"] = df[f"{challenge} - public"].rank(ascending=False).apply(lambda x: int(x) if not np.isnan(x) else 50)
 
         df[f"{challenge} - rank_score"] = df[f"{challenge} - rank"].apply(lambda x: np.round(math.log(x**2), 3) if x <= 50 else np.round(math.log(50**2), 3))
